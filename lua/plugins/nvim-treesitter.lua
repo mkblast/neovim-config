b868aa5627d341628ccaa5d45ef9e3c16bd989f6
Karim Mk <mk47blast@protonmail.com>
2023-06-26 16:20:15 +0100

    *

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,9 +1,14 @@
 return {
   'nvim-treesitter/nvim-treesitter',
 
+
+  dependencies = {
+    "HiPhish/nvim-ts-rainbow2"
+  },
+
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { "c", "python", "javascript", "html", "css", "lua" },
+      ensure_installed = { "c", "python", "javascript", "html", "css", "lua", "bash", "vim" },
       sync_install = true,
 
       highlight = {
@@ -11,11 +16,12 @@ return {
       },
       rainbow = {
         enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
+        -- list of languages you want to disable the plugin for
+        disable = { 'jsx', 'cpp' },
+        -- Which query to use for finding delimiters
+        query = 'rainbow-parens',
+        -- Highlight the entire buffer all at once
+        strategy = require('ts-rainbow').strategy.global,
       }
 
     }
