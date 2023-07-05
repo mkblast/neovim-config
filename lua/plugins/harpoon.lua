4bcdaba30d3714e23c1c004e5ce09765509d0cd2
Karim Mk <mk47blast@protonmail.com>
2023-07-05 22:58:34 +0100

    *

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -6,6 +6,7 @@ return {
     { mode = "n", '<leader>j', ':lua require("harpoon.ui").nav_file(2)<CR>',         { noremap = true, silent = true } },
     { mode = "n", '<leader>k', ':lua require("harpoon.ui").nav_file(3)<CR>',         { noremap = true, silent = true } },
     { mode = "n", '<leader>l', ':lua require("harpoon.ui").nav_file(4)<CR>',         { noremap = true, silent = true } },
+    { mode = "n", '<leader>;', ':lua require("harpoon.ui").nav_file(5)<CR>',         { noremap = true, silent = true } },
     { mode = "n", '<leader>i', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true } },
     { mode = "n", '<leader>o', ':lua require("harpoon.mark").add_file()<CR>',        { noremap = true, silent = true } },
   },
