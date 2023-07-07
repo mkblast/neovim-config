01810324f44e60c3eca1008970444aaca9606d85
Karim Mk <mk47blast@protonmail.com>
2023-07-07 20:01:44 +0100

    *

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,11 +2,15 @@ return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
-    { map = "n", "<leader>xx", "<cmd>TroubleToggle<cr>",                       { noremap = true, silent = true } },
-    { map = "n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { noremap = true, silent = true } },
-    { map = "n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",  { noremap = true, silent = true } },
-    { map = "n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",               { noremap = true, silent = true } },
-    { map = "n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",              { noremap = true, silent = true } },
-    { map = "n", "gR",         "<cmd>TroubleToggle lsp_references<cr>",        { noremap = true, silent = true } },
+    { mode = "n", "<leader>xx", "<cmd>TroubleToggle<cr>",                       { noremap = true, silent = true } },
+    { mode = "n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", { noremap = true, silent = true } },
+    { mode = "n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",  { noremap = true, silent = true } },
+    { mode = "n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",               { noremap = true, silent = true } },
+    { mode = "n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",              { noremap = true, silent = true } },
+    { mode = "n", "gR",         "<cmd>TroubleToggle lsp_references<cr>",        { noremap = true, silent = true } },
+  },
+
+  opts = {
+
   }
 }
