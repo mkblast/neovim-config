537e3899f354cc7795f9420c3035cfc814762857
Karim Mk <mk47blast@protonmail.com>
2023-07-12 16:54:52 +0100

    *

--- lua/settings.lua
+++ lua/settings.lua
@@ -29,11 +29,12 @@ opt.wrap = false
 opt.scrolloff = 8
 opt.cursorline = true
 opt.cursorcolumn = true
-opt.signcolumn= 'yes'
+opt.signcolumn = 'yes'
 opt.timeout = false
 opt.hlsearch = false
 opt.ignorecase = true
 opt.termguicolors = true
+opt.fillchars = { eob = " " }
 
 -- optimazations
 opt.hidden = true
@@ -78,28 +79,28 @@ opt.completeopt = 'menuone,noselect'
 -- startup
 -- disable builtins plugins
 local disabled_built_ins = {
-    "netrw",
-    "netrwPlugin",
-    "netrwSettings",
-    "netrwFileHandlers",
-    "gzip",
-    "zip",
-    "zipPlugin",
-    "tar",
-    "tarPlugin",
-    "getscript",
-    "getscriptPlugin",
-    "vimball",
-    "vimballPlugin",
-    "2html_plugin",
-    "logipat",
-    "rrhelper",
-    "spellfile_plugin",
-    "matchit"
+  "netrw",
+  "netrwPlugin",
+  "netrwSettings",
+  "netrwFileHandlers",
+  "gzip",
+  "zip",
+  "zipPlugin",
+  "tar",
+  "tarPlugin",
+  "getscript",
+  "getscriptPlugin",
+  "vimball",
+  "vimballPlugin",
+  "2html_plugin",
+  "logipat",
+  "rrhelper",
+  "spellfile_plugin",
+  "matchit"
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-    g["loaded_" .. plugin] = 1
+  g["loaded_" .. plugin] = 1
 end
 
 -- disable nvim intro
