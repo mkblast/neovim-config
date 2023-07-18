3c0dccb71d37c9d0f83048cfaea42ed5eb5b2fbd
Karim Mk <mk47blast@protonmail.com>
2023-07-18 22:16:55 +0100

    *

--- lua/plugins/nvim-colorizer.lua
+++ lua/plugins/nvim-colorizer.lua
@@ -2,6 +2,12 @@ return {
   "NvChad/nvim-colorizer.lua",
 
   config = function()
-    require 'colorizer'.setup()
+    require 'colorizer'.setup {
+      filetypes = {
+        'css',
+        'javascript',
+        'html',
+      },
+    }
   end
 }
