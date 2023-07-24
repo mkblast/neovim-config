29a977815e3010c775b3d5550b58026867f2bad5
Karim Mk <mk47blast@protonmail.com>
2023-07-24 17:31:01 +0100

    yeah the messages are usefull

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -2,62 +2,13 @@ return {
   "ThePrimeagen/harpoon",
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    {
-      mode = "n",
-      '<leader>h',
-      function()
-        require("harpoon.ui").nav_file(1)
-      end,
-      { noremap = true, silent = true }
-    },
-    {
-      mode = "n",
-      '<leader>j',
-      function()
-        require("harpoon.ui").nav_file(2)
-      end,
-      { noremap = true, silent = true }
-    },
-    {
-      mode = "n",
-      '<leader>k',
-      function()
-        require("harpoon.ui").nav_file(3)
-      end,
-      { noremap = true, silent = true }
-    },
-    {
-      mode = "n",
-      '<leader>l',
-      function()
-        require("harpoon.ui").nav_file(4)
-      end,
-      { noremap = true, silent = true }
-    },
-    {
-      mode = "n",
-      '<leader>;',
-      function()
-        require("harpoon.ui").nav_file(5)
-      end,
-      { noremap = true, silent = true }
-    },
-    {
-      mode = "n",
-      '<leader>i',
-      function()
-        require("harpoon.ui").toggle_quick_menu()
-      end,
-      { noremap = true, silent = true }
-    },
-    {
-      mode = "n",
-      '<leader>o',
-      function()
-        require("harpoon.mark").add_file()
-      end,
-      { noremap = true, silent = true }
-    },
+    { mode = "n", '<leader>h', ':lua require("harpoon.ui").nav_file(1)<CR>',         { noremap = true, silent = true } },
+    { mode = "n", '<leader>j', ':lua require("harpoon.ui").nav_file(2)<CR>',         { noremap = true, silent = true } },
+    { mode = "n", '<leader>k', ':lua require("harpoon.ui").nav_file(3)<CR>',         { noremap = true, silent = true } },
+    { mode = "n", '<leader>l', ':lua require("harpoon.ui").nav_file(4)<CR>',         { noremap = true, silent = true } },
+    { mode = "n", '<leader>;', ':lua require("harpoon.ui").nav_file(5)<CR>',         { noremap = true, silent = true } },
+    { mode = "n", '<leader>i', ':lua require("harpoon.ui").toggle_quick_menu()<CR>', { noremap = true, silent = true } },
+    { mode = "n", '<leader>o', ':lua require("harpoon.mark").add_file()<CR>',        { noremap = true, silent = true } },
   },
 
   config = {}
