a5c29d0d0bffff171767c09ebff500756508f20f
Karim Mk <mk47blast@protonmail.com>
2023-11-20 23:39:49 +0100

    *

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  tag = '0.1.1',
+  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
