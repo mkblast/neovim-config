53faf6a080bee196b2fb9a859f213a1abb683854
Karim Mk <mk47blast@protonmail.com>
2024-01-18 17:36:24 +0100

    Another binding change

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,26 +2,12 @@
 local map = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 
--- setting the leader key
-local g = vim.g
-g.mapleader = ' '
-
 -- don't use arrow keys
 map('', '<up>', '<nop>', default_opts)
 map('', '<down>', '<nop>', default_opts)
 map('', '<left>', '<nop>', default_opts)
 map('', '<right>', '<nop>', default_opts)
 
--- fast saving with <leader> and s
-map('n', '<leader>s', ':w<CR>', default_opts)
-map('n', '<leader>q', ':q<CR>', default_opts)
-map('n', '<leader>Q', ':qa<CR>', default_opts)
-
--- move around splits using Ctrl + {h,j,k,l}
-map('n', '<C-h>', '<C-w>h', default_opts)
-map('n', '<C-j>', '<C-w>j', default_opts)
-map('n', '<C-k>', '<C-w>k', default_opts)
-map('n', '<C-l>', '<C-w>l', default_opts)
 -- windows control
 map('n', '<leader>ws', ':split<CR>', default_opts)
 map('n', '<leader>wv', ':vsplit<CR>', default_opts)
@@ -32,8 +18,7 @@ map('n', '<leader>wh', ':hide<CR>', default_opts)
 
 -- buffer control
 map('n', '<leader>bd', ':bdelete!<CR>', default_opts)
-map('n', '<leader>bs', ':w<CR>:bdelete<CR>', default_opts)
-map('n', '<leader>j', '<c-^>', default_opts)
+map('n', '<leader>bs', ':w<cr>:bdelete<cr>', default_opts)
 map('n', '<TAB>', ':bnext<CR>', default_opts)
 map('n', '<S-TAB>', ':bprevious<CR>', default_opts)
 
@@ -49,7 +34,6 @@ map("n", "<C-d>", "<C-d>zz", default_opts)
 map("n", "<C-u>", "<C-u>zz", default_opts)
 map("n", "n", "nzzzv", default_opts)
 map("n", "N", "Nzzzv", default_opts)
-
 
 -- clipboard remaps
 map("x", "<leader>P", [["_dP]], default_opts)
